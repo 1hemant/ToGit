eea86e25d861986635f38926636e6526705bf49f
1hemant <hrathore@Netlinks-Mac-mini.local>
2015-12-30 12:45:43 +0530

    Working page
    Login page is working after reinstalling the bower components. Now I
    have to add background code.

--- LoginPage/js/login-page.js
+++ LoginPage/js/login-page.js
@@ -3,9 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-   $(window, document, undefined).ready(function() {
+$(window, document, undefined).ready(function() {
 
   $('input').blur(function() {
     var $this = $(this);
